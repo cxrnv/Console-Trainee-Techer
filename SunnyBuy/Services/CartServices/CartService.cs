bcf07260b5bbd916bdf2978ad8b2cce11b9d6b16
Trxuxv <deboraleticiaferreiramaciel@gmail.com>
2021-08-11 10:37:42 -0300

    11/08

--- SunnyBuy/Services/CartServices/CartService.cs
+++ SunnyBuy/Services/CartServices/CartService.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using SunnyBuy.Entitities.DB;
 using System.Collections.Generic;
 using SunnyBuy.Services.CartServices.Models;
+using SunnyBuy.Entitities;
 
 namespace SunnyBuy.Services.CartServices
 {
@@ -13,6 +14,11 @@ namespace SunnyBuy.Services.CartServices
         public CartService(SunnyBuy.Context.Context context)
         {
             this.context = context;
+        }
+
+        public CartService()
+        {
+
         }
 
         CartDB cart = new CartDB();
@@ -55,16 +61,38 @@ namespace SunnyBuy.Services.CartServices
         {
             var model = new Entitities.Cart()
             {
-                
+                ProductId = productId,
+                DateInclude = DateTime.Now,
+                Deleted = false
             };
 
-            /*
-            model.ProductId = productId;
-            model.DateInclude = DateTime.Now;
-            model.Deleted = false;
-            model.UserId = 1;
+            context.Cart.Add(model);
 
-            return context.Cart.Add(model);*/
+            return true;
+        }
+
+        public List<Entitities.Cart> show() 
+        {
+            var carts = new List<Entitities.Cart>();
+
+            carts.Where(a => a.ClientId == a.ClientId && !a.Deleted)
+                 .Select(b => new
+                 {
+                     b.CartId,
+                     b.ProductId,
+                     b.DateInclude
+                 }).ToList();
+
+            var products = new List<Entitities.Product>()
+                    .Where(a => carts.Any(c => c.ProductId == a.ProductId))
+                    .Select(b => new Entitities.Cart
+                    {
+                        ProductId = b.ProductId
+                    }
+                ).ToList(); 
+
+            return products;
+
         }
 
         public List<ListModel> ShowProductsCart()
